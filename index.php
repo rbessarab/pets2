1577dd571496250a98a610c284408c09e013c058
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-01-29 15:17:48 -0500

    added images folder, hom.html, and updated controller

--- index.php
+++ index.php
@@ -18,9 +18,8 @@ $f3->set('DEBUG', 3);
 
 //Define a default route (home page)
 $f3->route('GET /', function () {
-    echo "My pets";
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 $f3->run();
